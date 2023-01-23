26911c06535639e49888f160225c13e26c2fcc13
Hykudoru <iamalexfish@gmail.com>
2023-01-23 16:50:55 -0600

    Macro modes
    Need fix windows search

--- src/main.cpp
+++ src/main.cpp
@@ -35,13 +35,22 @@ Adafruit_SSD1306 oled = Adafruit_SSD1306(128, 32, &Wire);
 
 #if defined(ESP32)
   const int BAUD_RATE = 115200;
+  const uint16_t ADC_RESOLUTION = 4095; // 0 - 4095
   #define BUTTON_A 15
   #define BUTTON_B 32
   #define BUTTON_C 14
+  #define BUTTON_1 27
+  #define BUTTON_2 4
+  #define POTENTIOMETER_1 0b100111// A3 (39)
+  #define POTENTIOMETER_2 0b100100// A4 (36)
   const int POT_SENSOR_PIN_GPIO_39 = 39;
   const int TOUCH_PIN_GPIO_33 = 33;
   const int TOUCH_PIN_GPIO_27 = 27;
 #endif
+uint16_t rawPot1Value;
+uint16_t rawPot2Value;
+unsigned long* ptrPot1;
+unsigned long* ptrPot2;
 
 #if defined(__AVR_ATmega32U4__)
   const int BAUD_RATE = 9600;
@@ -100,57 +109,9 @@ void TriggerWakeSleepState()
   }
 }
 
-// ================================================================================
-//                                     SETUP       
-// ================================================================================
-void setup() 
-{ 
-  // put your setup code here, to run once:
-  Serial.begin(BAUD_RATE);
-
-  // =========================
-  //       SETUP DISPLAY
-  // =========================
-  oled.begin(SSD1306_SWITCHCAPVCC, 0x3C);
-  oled.display();//displays initial adafruit image
-  oled.clearDisplay();//clears initial adafruit image
-  oled.setTextSize(1);
-  oled.setTextColor(SSD1306_WHITE);
-  oled.setCursor(0, 0);
-
-  // =========================
-  //         SETUP IO
-  // =========================
-  pinMode(BUTTON_A, INPUT_PULLUP);
-  pinMode(BUTTON_B, INPUT_PULLUP);
-  pinMode(BUTTON_C, INPUT_PULLUP);
-  // touchAttachInterrupt(TOUCH_PIN_GPIO_33, TriggerWakeSleepState, 20);
-  // esp_sleep_enable_touchpad_wakeup();
-  leftJoystick.Start();
-  rightJoystick.Start();
-//=============================================================
-// The dual joystick controller uses two sparkfun joysticks 
-// oriented properly according to h and v axis marked on board.
-// If using one joystick on the index finger then the joystick 
-// is inverted AND then rotated. Later in the main loop the 
-// x and y axis are swaped a certain way depending on if 
-// left-handed or right-handed.
-//=============================================================
-  if (joystickCount < 2 ) {
-    leftJoystick.invertH = true;
-    leftJoystick.invertV = true;
-  }
-  //bleMouse.begin();
-  bleKeyboard.begin();
-
-  lastTimeIdle = millis();
-
-  oled.display();
- }
-
-// ==================================
-//         Keyboard Commands
-// ==================================
+// ================================================
+//         Keyboard Commands and Macros
+// ================================================
 
 void WindowsKey()
 {
@@ -186,16 +147,41 @@ void PasteCommand()
   bleKeyboard.releaseAll();
 } 
 
-void gpeditMacro()
+void CloseWindowForceQuit()
 {
-  const uint8_t str_gpedit[] = "gpedit";
-  //bleKeyboard.releaseAll();
+  bleKeyboard.press(KEY_LEFT_ALT);
+  bleKeyboard.press(KEY_F4);
+  delay(100);
+  bleKeyboard.releaseAll();
+}
 
+void WindowsSearchRun(const uint8_t *str_search)
+{
+  //bleKeyboard.releaseAll();
   // OPEN GPEDIT: (WinKey > type "gpedit" > Enter).
   WindowsKey(); 
   delay(1000); // wait for home menu
-  bleKeyboard.write(str_gpedit, sizeof(str_gpedit)); delay(100);
+  bleKeyboard.write(str_search, sizeof(str_search)); delay(100);
   EnterKey();
+}
+
+void OpenCMD()
+{
+  const uint8_t cmd[] = "cmd";
+  WindowsSearchRun(cmd);
+}
+
+void GpeditMacro()
+{
+  oled.clearDisplay();
+  oled.println(String("Running gpedit macro..."));
+  oled.display();
+  
+  const uint8_t gpedit[] = "gpedit";
+  //bleKeyboard.releaseAll();
+
+  // OPEN GPEDIT: (WinKey > type "gpedit" > Enter).
+  WindowsSearchRun(gpedit);
   delay(2500); // wait for gpedit window
 
   /*----------------------------------------
@@ -240,6 +226,130 @@ void gpeditMacro()
 
 }
 
+void BitlockerMacro()
+
+{
+  oled.clearDisplay();
+  oled.println(String("Running Bitlocker macro..."));
+  oled.display();
+
+  const uint8_t bitlocker[] = "bitlocker";
+  WindowsSearchRun(bitlocker);
+  delay(2500); // wait for bitlocker
+
+  for (size_t i = 0; i < 9; i++)
+  {
+    bleKeyboard.write(KEY_TAB);
+  }
+  EnterKey();
+  EnterKey();
+
+  //To-Do...
+}
+
+void GpeditMode() 
+{
+  oled.clearDisplay();
+  oled.setCursor(0, 0);
+  oled.println(String("gpedit Macro"));
+  oled.display();
+
+  if (leftJoystick.Read().z > 0 && rightJoystick.Read().z > 0)
+  {
+    GpeditMacro(); //run macro
+  }
+
+  delay(10);
+}
+
+void BitLockerMode() 
+{
+  oled.clearDisplay();
+  oled.setCursor(0, 0);
+  oled.println(String("BitLocker Macro"));
+  oled.display();
+  
+  if (leftJoystick.Read().z > 0 && rightJoystick.Read().z > 0)
+  {
+    BitlockerMacro(); //run macro
+  }
+
+  delay(10);
+}
+
+static pointerFunction modes[] = {GpeditMode, BitLockerMode};
+static int modeIndex = 0;
+static int prevModeIndex = 0;
+
+void OnClickButton1() 
+{
+  modeIndex = clamp(--modeIndex, 0, 3);
+  ptrMode = modes[modeIndex];
+  Serial.println("Button Pressed");
+}
+
+void OnClickButton2() 
+{
+  modeIndex = clamp(++modeIndex, 0, 3);
+  ptrMode = modes[modeIndex];
+  Serial.println("Button 2 Pressed");
+}
+
+// ================================================================================
+//                                     SETUP       
+// ================================================================================
+void setup() 
+{ 
+  // put your setup code here, to run once:
+  Serial.begin(BAUD_RATE);
+
+  // =========================
+  //       SETUP DISPLAY
+  // =========================
+  oled.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+  oled.display();//displays initial adafruit image
+  oled.clearDisplay();//clears initial adafruit image
+  oled.setTextSize(1);
+  oled.setTextColor(SSD1306_WHITE);
+  oled.setCursor(0, 0);
+
+  // =========================
+  //         SETUP IO
+  // =========================
+  pinMode(BUTTON_A, INPUT_PULLUP);
+  pinMode(BUTTON_B, INPUT_PULLUP);
+  pinMode(BUTTON_C, INPUT_PULLUP);
+  pinMode(BUTTON_1, INPUT_PULLDOWN);
+  pinMode(BUTTON_2, INPUT_PULLDOWN);
+  // RISING & FALLING are reversed if INPUT_PULLDOWN
+  attachInterrupt(BUTTON_1, OnClickButton1, RISING);
+  attachInterrupt(BUTTON_2, OnClickButton2, RISING);
+  
+  //touchAttachInterrupt(TOUCH_PIN_GPIO_33, TriggerWakeSleepState, 20);
+  // esp_sleep_enable_touchpad_wakeup();
+  leftJoystick.Start();
+  rightJoystick.Start();
+//=============================================================
+// The dual joystick controller uses two sparkfun joysticks 
+// oriented properly according to h and v axis marked on board.
+// If using one joystick on the index finger then the joystick 
+// is inverted AND then rotated. Later in the main loop the 
+// x and y axis are swaped a certain way depending on if 
+// left-handed or right-handed.
+//=============================================================
+  if (joystickCount < 2 ) {
+    leftJoystick.invertH = true;
+    leftJoystick.invertV = true;
+  }
+  //bleMouse.begin();
+  bleKeyboard.begin();
+
+  ptrMode = *modes;//GpeditMode;
+  
+  lastTimeIdle = millis();
+
+  oled.display();
+ }
 // =======================================================================================
 //                                    MAIN PROGRAM                            
 // =======================================================================================
@@ -304,10 +414,9 @@ void loop()
 
     if (bleKeyboard.isConnected())
     {
-     if (mouse.z && mouse2.z)
-      { 
-        gpeditMacro(); //run macro
-      }
+        if (ptrMode) {
+          (*ptrMode)();
+        }
     }
 
     // if (bleMouse.isConnected()) 
